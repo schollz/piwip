db14fe6d0b8d3a493b47c3036965815f307776c5
Zack Scholl <zack.scholl@gmail.com>
2020-09-24 10:17:18 -0700

    initialize softcut

--- prism.lua
+++ prism.lua
@@ -28,6 +28,8 @@ function init()
   for i=1,6 do
     s.v[i]={}
     s.v[i].position=0
+    s.v[i].midi=0 -- target midi note
+    s.v[i].freq=0 -- current frequency
   end
   
   -- initialize timers
@@ -43,14 +45,12 @@ function init()
   softcut.poll_start_phase()
   
   -- frequency poll
-  --update pitch
   local pitch_poll_l=poll.set("pitch_in_l",function(value)
     update_freq(value)
   end)
   pitch_poll_l:start()
   
-  -- listen to audio
-  -- and initiate recording on incoming audio
+  -- amplitude poll
   p_amp_in=poll.set("amp_in_l")
   p_amp_in.time=params:get("resolution")/1000
   p_amp_in.callback=function(val)
@@ -62,6 +62,37 @@ function init()
   midi_signal_in=midi.connect(1)
   midi_signal_in.event=update_midi
   
+  -- initialize softcut
+  for i=1,6 do
+    if i==1 then
+      -- voice 1 records into buffer, but does not play
+      softcut.level(i,0)
+      softcut.level_input_cut(1,i,1)
+      softcut.level_input_cut(2,i,1)
+      softcut.rec_level(i,1)
+      softcut.pre_level(i,0)
+    else
+      softcut.level(i,1)
+      softcut.level_input_cut(1,i,0)
+      softcut.level_input_cut(2,i,0)
+    end
+    softcut.pan(i,0)
+    softcut.play(i,0)
+    softcut.rate(i,1)
+    softcut.loop_start(i,0)
+    softcut.loop_end(i,120)
+    softcut.loop(i,1)
+    softcut.rec(i,0)
+    
+    softcut.fade_time(i,0.2)
+    softcut.level_slew_time(i,params:get("resolution")/1000)
+    softcut.rate_slew_time(i,params:get("resolution")/1000)
+    
+    softcut.buffer(i,1)
+    softcut.position(i,0)
+    softcut.enable(i,1)
+    softcut.phase_quant(i,params:get("resolution")/1000/2)
+  end
 end
 
 --
@@ -86,14 +117,32 @@ function update_main()
   if s.update_main then
     redraw()
   end
+  for i=1,6 do
+    if s.v[i].midi>0 then
+      -- TODO: modulate the voice's rate to match upcoming pitch
+      -- TODO: if midi is higher then pitch, then find the next octave down
+    end
+  end
 end
 
 function update_midi(data)
   msg=midi.to_msg(data)
   if msg.type=='note_on' then
-    hz=(440/32)*(2^((msg.note-9)/12))
     print(msg.note,msg.vel/127.0)
+    -- find first available voice and turn it on
+    for i=1,6 do
+      if s.v[i].midi==0 then
+        s.v[i].midi=msg.note
+        break
+      end
+    end
   elseif msg.type=='note_off' then
+    -- turn off any voices on that note
+    for i=1,6 do
+      if s.v[i].midi==msg.note then
+        s.v[i].midi=0
+      end
+    end
   end
 end
 
@@ -129,4 +178,8 @@ function round_to_nearest(x,yth)
     return x
   end
   return x+yth-remainder
+end
+
+function midi_to_hz(note)
+  return (440/32)*(2^((note-9)/12))
 end
